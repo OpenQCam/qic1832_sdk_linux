0a985f3ed594237352aa1b2ec289b9b98561c9fa
Yang Chang <yang.chang@quantatw.com>
2016-01-21 14:12:41 +0800

    -n to select device node in example_avc -o to output timestamp information in millisecond

--- qic_example/example_avconly.c
+++ qic_example/example_avconly.c
@@ -43,12 +43,12 @@ char *debug_level[] = {"INFO", "WARN", "CRIT", "FATL", "DEAD"};
 
 void debug_log(int level, char *string) {
 
-		printf("QIC debug_print (%s):%s", debug_level[level], string);
+    printf("QIC debug_print (%s):%s", debug_level[level], string);
 }
 
 void frame_process(unsigned int dev_id,  out_frame_t frame) {
 
-	static unsigned int avc_count = 0;
+  static unsigned int avc_count = 0;
 
 /* { calculate current frame rate*/
 #ifdef CALC_FPS
@@ -61,242 +61,249 @@ float fps;
 #endif
 /*  calculate current frame rate}*/
 
-	switch (dev_id) {
+  switch (dev_id) {
 
-		case DEV_ID_0:
+    case DEV_ID_0:
 
-			if(frame_count > 0)
-			{
-			avc_count++;
-			write_avc(frame.frame_data,frame.frame_len);			
-			putchar('C');fflush(stdout);
-                	}
+      if(frame_count > 0)
+      {
+      avc_count++;
+      write_avc(frame.frame_data,frame.frame_len);
+      putchar('C');fflush(stdout);
+                  }
 
 /*  {calculate current frame rate*/
 #ifdef CALC_FPS
 
-	frame_num++;
+  frame_num++;
 if(frame_num>=30){
-	clock_gettime(0,&start_ts);
-	diff_sec=start_ts.tv_sec- end_ts.tv_sec;
-	diff_nsec=start_ts.tv_nsec-end_ts.tv_nsec;
-	diff_msec=(diff_sec*1000.)+(diff_nsec/1000000.);
-	fps=(frame_num*1000.)/diff_msec;
-	printf("(H264) frame rate= %2.2f fps \n", fps);
+  clock_gettime(0,&start_ts);
+  diff_sec=start_ts.tv_sec- end_ts.tv_sec;
+  diff_nsec=start_ts.tv_nsec-end_ts.tv_nsec;
+  diff_msec=(diff_sec*1000.)+(diff_nsec/1000000.);
+  fps=(frame_num*1000.)/diff_msec;
+  printf("(H264) frame rate= %2.2f fps \n", fps);
 
-	frame_num=0;
+  frame_num=0;
 
-	clock_gettime(0,&end_ts);
-	
+  clock_gettime(0,&end_ts);
+
 }
 #endif
-/*  calculate current frame rate}*/	
+/*  calculate current frame rate}*/
 
-			break;
+      break;
 
-	}
+  }
 
-	frame_count--;
-	if((avc_count)%100 == 0)
-		printf("\n");	
+  frame_count--;
+  if((avc_count)%100 == 0)
+    printf("\n");
 
 }
 
 
 static void usage(FILE * fp, int argc, char **argv)
 {
-	fprintf(fp,
-	"Usage: %s [options]\n\n"
-	"Options:\n"
-	"-o | --output		H.264/AVC output [filename]\n"
-	"-s | --AVCsize		H.264/AVC stream [width]x[height]\n"
-	"-f | --fps		Framerate\n"
-	"-b | --bitrate	        Bitrate\n"
-	"-g | --gop		GOP value\n"
-	"-c | --count		Capture Counter\n"
-	"-d | --demux		H264 bad frame check\n"
-	"-h | --help		Print this message\n"
-	"",
-	argv[0]);
+  fprintf(fp,
+  "Usage: %s [options]\n\n"
+  "Options:\n"
+  "-o | --output    H.264/AVC output [filename]\n"
+  "-n | --device node H.264/AVC device [devicenode]\n"
+  "-s | --AVCsize   H.264/AVC stream [width]x[height]\n"
+  "-f | --fps   Framerate\n"
+  "-b | --bitrate         Bitrate\n"
+  "-g | --gop   GOP value\n"
+  "-c | --count   Capture Counter\n"
+  "-d | --demux   H264 bad frame check\n"
+  "-h | --help    Print this message\n"
+  "",
+  argv[0]);
 }
 
 
 
-static const char short_options [] = "o:s:f:b:g:c:hd";
+static const char short_options [] = "o:s:f:b:g:c:dh:n:";
 
 
 static const struct option long_options [] =
 {
-	{ "output",		required_argument,	NULL,	'o' },
-	{ "AVCsize",		required_argument,	NULL,	's' },
-	{ "fps",		required_argument,	NULL,	'f' },
-	{ "bitrate",		required_argument,	NULL,	'b' },
-	{ "gop",		required_argument,	NULL,	'g' },
-	{ "count",		required_argument,	NULL,	'c' },
-	{ "demux",		no_argument,	NULL,	'd' },
-	{ "help",		no_argument,		NULL,	'h' },
-
-	{ 0, 0, 0, 0 }
+  { "output",   required_argument,  NULL, 'o' },
+  { "AVCsize",    required_argument,  NULL, 's' },
+  { "fps",    required_argument,  NULL, 'f' },
+  { "bitrate",    required_argument,  NULL, 'b' },
+  { "gop",    required_argument,  NULL, 'g' },
+  { "count",    required_argument,  NULL, 'c' },
+  { "demux",    no_argument,  NULL, 'd' },
+  { "help",   no_argument,    NULL, 'h' },
+  { "devicenode", required_argument,  NULL, 'n' },
+  { 0, 0, 0, 0 }
 };
 
 
 int main(int argc,char ** argv)
-{	
-	char *separator;
-	char *sizestring = NULL;
-	char *filename = NULL;
+{
+  char *separator;
+  char *sizestring = NULL;
+  char *filename = NULL;
+  char *devicenode = NULL;
 
-	unsigned short avc_width = 1280;
-	unsigned short avc_height = 720;
-	unsigned char u_framerate = 30, demux=0;
-	unsigned int u_bitrate = 5000000;
-	unsigned int u_frame_interval = 333333; //frame rate interval, unit:100nS ->0.1uS
-	unsigned short u_key_frame_interval = 0; //mS gop interval
-	qic_module *my_qic = NULL;
-	int ret;
+  unsigned short avc_width = 1280;
+  unsigned short avc_height = 720;
+  unsigned char u_framerate = 30, demux=0;
+  unsigned int u_bitrate = 5000000;
+  unsigned int u_frame_interval = 333333; //frame rate interval, unit:100nS ->0.1uS
+  unsigned short u_key_frame_interval = 0; //mS gop interval
+  qic_module *my_qic = NULL;
+  int ret;
 
-	qic_dev_name_s video_name;
+  qic_dev_name_s video_name;
 
-	if (getuid() != 0){
-		printf("please run as root\n");
-		exit(0);
-	}
+  if (getuid() != 0){
+    printf("please run as root\n");
+    exit(0);
+  }
 
-	memset(&video_name,0, sizeof(video_name));
-	if(qic_enum_device_formats(&video_name)){
-		printf("Error!! Not supported video device\n");
-		return 1;
-	}
-	printf("encdoing video=%s, raw video=%s\n",video_name.dev_avc, video_name.dev_yuv);
+  memset(&video_name,0, sizeof(video_name));
+  if(qic_enum_device_formats(&video_name)){
+    printf("Error!! Not supported video device\n");
+    return 1;
+  }
+  printf("encdoing video=%s, raw video=%s\n",video_name.dev_avc, video_name.dev_yuv);
 
-	for (;;)
-	{
-		int index;
-		int c;
+  for (;;)
+  {
+    int index;
+    int c;
 
-		c = getopt_long(argc, argv,
-		                short_options, long_options,
-		                &index);
+    c = getopt_long(argc, argv,
+                    short_options, long_options,
+                    &index);
 
-		if (-1 == c)
-			break;
+    if (-1 == c)
+      break;
 
-		switch (c)
-		{
+    switch (c)
+    {
 
-		case 0: /* getopt_long() flag */
-			break;
+    case 0: /* getopt_long() flag */
+      break;
 
 
-		case 'o':
-			filename = optarg;
-			break;
+    case 'o':
+      filename = optarg;
+      break;
+    case 'n':
+//devicenode = argv[index-1];
+      devicenode = optarg;
+      printf("i got n %s\n", devicenode);
+      break;
+    case 's':
+      sizestring = strdup(optarg);
+      avc_width = strtoul(sizestring, &separator, 10);
+      if (*separator != 'x') {
+        printf("Error in size use -s widthxheight\n");
+        exit(1);
+      } else {
+        ++separator;
+        avc_height = strtoul(separator, &separator, 10);
+        if (*separator != 0)
+          printf("hmm.. dont like that!! trying this height\n");
+      }
+      break;
+    case 'f':
+      u_framerate = atoi(optarg);
+      u_frame_interval=10000000/u_framerate;
+      printf("Set u_framerate=%d...\n",u_framerate);
+      break;
 
-	        case 's':
-			sizestring = strdup(optarg);
-			avc_width = strtoul(sizestring, &separator, 10);
-			if (*separator != 'x') {
-				printf("Error in size use -s widthxheight\n");
-				exit(1);
-			} else {
-				++separator;
-				avc_height = strtoul(separator, &separator, 10);
-				if (*separator != 0)
-					printf("hmm.. dont like that!! trying this height\n");
-			}
-			break;
-			
-		case 'f':
-			u_framerate = atoi(optarg);
-		 	u_frame_interval=10000000/u_framerate;
-		 	printf("Set u_framerate=%d...\n",u_framerate);
-			break;
-			
-		case 'b':
-			u_bitrate = atoi(optarg);
-			printf("Set u_bitrate=%d...\n",u_bitrate);
-			break;
-			
-		case 'g':
-			u_key_frame_interval = atoi(optarg);
-			printf("Set u_key_frame_interval=%d...\n",u_key_frame_interval);
-			break;
-			
-		case 'c':
-			frame_count= atoi(optarg);
-			printf("Set frame_count=%d...\n",frame_count);
-			break;
+    case 'b':
+      u_bitrate = atoi(optarg);
+      printf("Set u_bitrate=%d...\n",u_bitrate);
+      break;
 
-		case 'd':
-			printf("Enable H264 bad frame check...\n");
-			demux=1;
-			break;
+    case 'g':
+      u_key_frame_interval = atoi(optarg);
+      printf("Set u_key_frame_interval=%d...\n",u_key_frame_interval);
+      break;
 
-		case 'h':
-			usage(stdout, argc, argv);
-			exit(EXIT_SUCCESS);
-			
-		default:
-			usage(stderr, argc, argv);
-			exit(EXIT_FAILURE);
-		}
-	}
+    case 'c':
+      frame_count= atoi(optarg);
+      printf("Set frame_count=%d...\n",frame_count);
+      break;
 
-	
+    case 'd':
+      printf("Enable H264 bad frame check...\n");
+      demux=1;
+      break;
+
+    case 'h':
+      usage(stdout, argc, argv);
+      exit(EXIT_SUCCESS);
+
+    default:
+      usage(stderr, argc, argv);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+
 /************************************************
  *
  * first step, init the qic module capture library
  * two devices, /dev/video0 as YUV raw
  *
  *************************************************/
-	my_qic = qic_initialize(1);
+  my_qic = qic_initialize(1);
 
-	if (my_qic == NULL) {
-		printf("qic_initialize error\n");
-		return 1;
-	}
+  if (my_qic == NULL) {
+    printf("qic_initialize error\n");
+    return 1;
+  }
 
 
-	
+
 /************************************************
  *
  * step 2: set the parameters and commit the settings
  * need to setup two call back functions, debug_print & frame_output
  *
  *************************************************/
-	/* call back functions */
-	my_qic->debug_print = &debug_log;
-	my_qic->frame_output2 = &frame_process;
+  /* call back functions */
+  my_qic->debug_print = &debug_log;
+  my_qic->frame_output2 = &frame_process;
 
-	/*  set scheduler */
-	my_qic->high_prio = 0;
+  /*  set scheduler */
+  my_qic->high_prio = 0;
 
-	/*set /dev/video1 as device 1 (H.264/AVC) */
-	if(strlen( video_name.dev_avc)>0)
-		my_qic->cam[0].dev_name =  video_name.dev_avc;
-    else	
-    	my_qic->cam[0].dev_name ="/dev/video1";
-	my_qic->cam[0].format = V4L2_PIX_FMT_MJPEG;
-	my_qic->cam[0].bitrate = u_bitrate;
-	my_qic->cam[0].width = avc_width;
-	my_qic->cam[0].height = avc_height;
-	my_qic->cam[0].framerate = u_framerate;
-	my_qic->cam[0].codec_type=CODEC_H264;
-	my_qic->cam[0].is_encoding_video=1;
-	my_qic->cam[0].key_frame_interval=u_key_frame_interval;
-	my_qic->cam[0].frame_interval=u_frame_interval;
-	if(demux){
-		my_qic->cam[0].is_demux =1;  //Enable Encoding stream bad frame check
-	}
+  /*set /dev/video1 as device 1 (H.264/AVC) */
+  if(strlen( video_name.dev_avc)>0 && devicenode==NULL)
+    my_qic->cam[0].dev_name =  video_name.dev_avc;
+  else if(devicenode!=NULL)
+    my_qic->cam[0].dev_name =devicenode;
+  else
+    my_qic->cam[0].dev_name ="/dev/video1";
+  my_qic->cam[0].format = V4L2_PIX_FMT_MJPEG;
+  my_qic->cam[0].bitrate = u_bitrate;
+  my_qic->cam[0].width = avc_width;
+  my_qic->cam[0].height = avc_height;
+  my_qic->cam[0].framerate = u_framerate;
+  my_qic->cam[0].codec_type=CODEC_H264;
+  my_qic->cam[0].is_encoding_video=1;
+  my_qic->cam[0].key_frame_interval=u_key_frame_interval;
+  my_qic->cam[0].frame_interval=u_frame_interval;
+  if(demux){
+    my_qic->cam[0].is_demux =1;  //Enable Encoding stream bad frame check
+  }
 
 
 
-	/* commit and init the video dev */
-	ret = qic_config_commit();
-	if (ret) {
-		printf("qic_config_commit error\n");
-		return 1;
-	}
+  /* commit and init the video dev */
+  ret = qic_config_commit();
+  if (ret) {
+    printf("qic_config_commit error\n");
+    return 1;
+  }
 
  qic_config_codec_EU(DEV_ID_0,CODEC_H264);
  qic_change_resolution_EU(DEV_ID_0, SIMULCAST_STREAM0, avc_width, avc_height);
@@ -311,12 +318,12 @@ int main(int argc,char ** argv)
  * step 3: config print debug can be called at any time
  *
  *************************************************/
-	char *log_str = NULL;
-	log_str = qic_print_config_param(DEV_ID_0); /* print both interface config */
-	printf("%s", log_str);
+  char *log_str = NULL;
+  log_str = qic_print_config_param(DEV_ID_0); /* print both interface config */
+  printf("%s", log_str);
 
-	/* open file for dump */
-	open_file_dump(filename);
+  /* open file for dump */
+  open_file_dump(filename);
 
 
 /************************************************
@@ -324,11 +331,11 @@ int main(int argc,char ** argv)
  * step 4: activate the camera before capture frames
  *
  *************************************************/
-	ret = qic_start_capture(DEV_ID_0); /* activate both video0 & video1 */
-	if (ret) {
-		printf("qic_start_capture error\n");
-		return 1;
-	}
+  ret = qic_start_capture(DEV_ID_0); /* activate both video0 & video1 */
+  if (ret) {
+    printf("qic_start_capture error\n");
+    return 1;
+  }
 
 /************************************************
  *
@@ -336,57 +343,57 @@ int main(int argc,char ** argv)
  *             multiplex with other i/o
  *
  *************************************************/
-	int fd0 = qic_get_fd_from_devid(DEV_ID_0);
-	int max_fd = fd0+1;
+  int fd0 = qic_get_fd_from_devid(DEV_ID_0);
+  int max_fd = fd0+1;
 
-	/* custom select function for capture frames without pthread */
-	fd_set fds;
-	
-	struct timeval tv;
-	int r;
+  /* custom select function for capture frames without pthread */
+  fd_set fds;
 
-	unsigned int test_value=0;
+  struct timeval tv;
+  int r;
 
-	while (frame_count > 0) {
+  unsigned int test_value=0;
 
-		FD_ZERO(&fds);
-		FD_SET(fd0, &fds);
+  while (frame_count > 0) {
 
-		/* Timeout. */
-		tv.tv_sec = 5;
-		tv.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(fd0, &fds);
 
-		r = select(max_fd, &fds, NULL, NULL, &tv);
-		
-		if (-1 == r) {
-			if (EINTR == errno)
-				continue;
-		
-			printf("select error %d, %s\n", errno, strerror(errno));
-			exit(1);
-		}
-		
-		if (0 == r) {
-			printf("select timeout\n");
-			exit(1);
-		}
+    /* Timeout. */
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
 
-		if (FD_ISSET(fd0, &fds)) {
-			ret = qic_getframe2(DEV_ID_0);
+    r = select(max_fd, &fds, NULL, NULL, &tv);
 
-			if (ret) {
-				printf("qic_getframe error\n");
-				return 1;
-			}
+    if (-1 == r) {
+      if (EINTR == errno)
+        continue;
 
-		}
+      printf("select error %d, %s\n", errno, strerror(errno));
+      exit(1);
+    }
 
-	}
+    if (0 == r) {
+      printf("select timeout\n");
+      exit(1);
+    }
 
-	/* print the mem usage */
-	char *mem_info = NULL;
-	mem_info = mem_usage_info();
-	printf("%s", mem_info);
+    if (FD_ISSET(fd0, &fds)) {
+      ret = qic_getframe2(DEV_ID_0);
+
+      if (ret) {
+        printf("qic_getframe error\n");
+        return 1;
+      }
+
+    }
+
+  }
+
+  /* print the mem usage */
+  char *mem_info = NULL;
+  mem_info = mem_usage_info();
+  printf("%s", mem_info);
 
 
 /************************************************
@@ -394,27 +401,27 @@ int main(int argc,char ** argv)
  * step 6: stop the camera
  *
  *************************************************/
-	ret = qic_stop_capture(DEV_ID_0); /* deactivate both video0 & video1 */
-	if (ret) {
-		printf("qic_stop_capture error\n");
-		return 1;
-	}
+  ret = qic_stop_capture(DEV_ID_0); /* deactivate both video0 & video1 */
+  if (ret) {
+    printf("qic_stop_capture error\n");
+    return 1;
+  }
 
-	
+
 /************************************************
  *
  * step 7: release all fd and malloc(s) before exit
   *
  *************************************************/
-	ret = qic_release();
-	if (ret) {
-		printf("qic_release error\n");
-		return 1;
-	}
+  ret = qic_release();
+  if (ret) {
+    printf("qic_release error\n");
+    return 1;
+  }
 
 
-	close_file();
+  close_file();
 
 
-	return 0;
+  return 0;
 }
--- qic_example/misc_writefile.c
+++ qic_example/misc_writefile.c
@@ -24,6 +24,7 @@
 #ifdef USE_FWRITE
 /*FILE *file = NULL;*/
 FILE *file_264 = NULL;
+FILE *file_ts  = NULL;
 FILE *file_yuv = NULL;
 FILE *file_mjpeg = NULL;
 FILE *file_ivf = NULL;
@@ -31,6 +32,7 @@ FILE *file_log = NULL;
 #else
 /*int file = -1;*/
 int file_264 = -1;
+int file_ts = -1;
 int file_yuv= -1;
 int file_mjpeg = -1;
 int file_ivf = -1;
@@ -43,49 +45,49 @@ int file_log = -1;
 #define DWORD  unsigned int
 #endif
 
-typedef struct 
+typedef struct
 {
-	DWORD signature;
-	WORD  version;
-	WORD  length;
-	DWORD fourcc;
-	WORD  width;
-	WORD  height;
-	DWORD framerate;
-	DWORD timescale;
-	DWORD num_of_frames;
-	DWORD unused;
+  DWORD signature;
+  WORD  version;
+  WORD  length;
+  DWORD fourcc;
+  WORD  width;
+  WORD  height;
+  DWORD framerate;
+  DWORD timescale;
+  DWORD num_of_frames;
+  DWORD unused;
 }IVFFileHeader;
 
 typedef struct
 {
-	DWORD size;
-	long timestamp;
+  DWORD size;
+  long timestamp;
 }IVFFrameHeader;
 
 
 
 static void errno_exit(const char*  s)
 {
-	fprintf(stderr, "%s error %d, %s\n",
-	        s, errno, strerror(errno));
+  fprintf(stderr, "%s error %d, %s\n",
+          s, errno, strerror(errno));
 
-	exit(EXIT_FAILURE);
+  exit(EXIT_FAILURE);
 }
 
 #if 0
 void write_file (char* data, int length) {
 
 #ifdef USE_FWRITE
-	if (file != NULL) {
-		if( -1 == fwrite(data, length, 1, file))
-			errno_exit("write_file_error");
-	}
+  if (file != NULL) {
+    if( -1 == fwrite(data, length, 1, file))
+      errno_exit("write_file_error");
+  }
 #else
-	if (file != -1) {
-		if( -1 == write(file, data, length))
-			errno_exit("write_file_error");
-	}
+  if (file != -1) {
+    if( -1 == write(file, data, length))
+      errno_exit("write_file_error");
+  }
 
 #endif
 
@@ -93,47 +95,47 @@ void write_file (char* data, int length) {
 #endif
 
 void add_vp8_file_header(unsigned short width, unsigned short height,unsigned int framerate, int nof)
-{		
+{
                 IVFFileHeader header;
-		header.signature = 'FIKD';
-		header.version = 0;
-		header.length = sizeof(header);
-		header.fourcc = '08PV';
-		header.width = width;
-		header.height = height;
-		header.framerate = framerate;
-		header.timescale = 1;
-		header.num_of_frames = nof;
-		header.unused=0;
+    header.signature = 'FIKD';
+    header.version = 0;
+    header.length = sizeof(header);
+    header.fourcc = '08PV';
+    header.width = width;
+    header.height = height;
+    header.framerate = framerate;
+    header.timescale = 1;
+    header.num_of_frames = nof;
+    header.unused=0;
 #ifdef USE_FWRITE
-		if (file_ivf != NULL) {
-		if( -1 == fwrite(&header, 32, 1, file_ivf))
-			errno_exit("write_file_error");
-		}
+    if (file_ivf != NULL) {
+    if( -1 == fwrite(&header, 32, 1, file_ivf))
+      errno_exit("write_file_error");
+    }
 #else
-	if (file_ivf != -1) {
-		if( -1 == write(file_ivf, &header, 32))
-			errno_exit("write_file_error");
-	}
+  if (file_ivf != -1) {
+    if( -1 == write(file_ivf, &header, 32))
+      errno_exit("write_file_error");
+  }
 
 #endif
 }
 
 void add_vp8_frame_header(int frame_len, long timestamp)
 {
-	    static IVFFrameHeader frame_header;	
-		frame_header.size = frame_len;
-		frame_header.timestamp = timestamp;
+      static IVFFrameHeader frame_header;
+    frame_header.size = frame_len;
+    frame_header.timestamp = timestamp;
 #ifdef USE_FWRITE
-		if (file_ivf != NULL) {
-		if( -1 == fwrite(&frame_header, 12, 1, file_ivf))
-			errno_exit("write_file_error");
-		}
+    if (file_ivf != NULL) {
+    if( -1 == fwrite(&frame_header, 12, 1, file_ivf))
+      errno_exit("write_file_error");
+    }
 #else
-	if (file_ivf != -1) {
-		if( -1 == write(file_ivf, &frame_header, 12))
-			errno_exit("write_file_error");
-	}
+  if (file_ivf != -1) {
+    if( -1 == write(file_ivf, &frame_header, 12))
+      errno_exit("write_file_error");
+  }
 
 #endif
 
@@ -142,15 +144,26 @@ void add_vp8_frame_header(int frame_len, long timestamp)
 void write_avc (char* data, int length) {
 
 #ifdef USE_FWRITE
-	if (file_264 != NULL) {
-		if( -1 == fwrite(data, length, 1, file_264))
-			errno_exit("write_file_error");
-	}
+  if (file_264 != NULL) {
+    if( -1 == fwrite(data, length, 1, file_264))
+      errno_exit("write_file_error");
+  }
+  struct timeval tv;
+  gettimeofday(&tv,NULL);
+  unsigned long current_ts=((tv.tv_sec*1000)+(tv.tv_usec/1000));
+  if (file_ts != NULL) {
+    if( fprintf(file_ts, "%d %lu\n",length, current_ts) < 0 )
+      errno_exit("write_file_error");
+  }
 #else
-	if (file_264 != -1) {
-		if( -1 == write(file_264, data, length))
-			errno_exit("write_file_error");
-	}
+  if (file_264 != -1) {
+    if( -1 == write(file_264, data, length))
+      errno_exit("write_file_error");
+  }
+  //TODO write with "write" function
+  /*if (file_ts != NULL) {
+
+  }*/
 
 #endif
 
@@ -159,15 +172,15 @@ void write_avc (char* data, int length) {
 void write_yuv (char* data, int length) {
 
 #ifdef USE_FWRITE
-	if (file_yuv != NULL) {
-		if( -1 == fwrite(data, length, 1, file_yuv))
-			errno_exit("write_file_error");
-	}
+  if (file_yuv != NULL) {
+    if( -1 == fwrite(data, length, 1, file_yuv))
+      errno_exit("write_file_error");
+  }
 #else
-	if (file_yuv != -1) {
-		if( -1 == write(file_yuv, data, length))
-			errno_exit("write_file_error");
-	}
+  if (file_yuv != -1) {
+    if( -1 == write(file_yuv, data, length))
+      errno_exit("write_file_error");
+  }
 
 #endif
 
@@ -176,15 +189,15 @@ void write_yuv (char* data, int length) {
 void write_mjpeg(char* data, int length) {
 
 #ifdef USE_FWRITE
-	if (file_mjpeg != NULL) {
-		if( -1 == fwrite(data, length, 1, file_mjpeg))
-			errno_exit("write_file_error");
-	}
+  if (file_mjpeg != NULL) {
+    if( -1 == fwrite(data, length, 1, file_mjpeg))
+      errno_exit("write_file_error");
+  }
 #else
-	if (file_mjpeg != -1) {
-		if( -1 == write(file_mjpeg, data, length))
-			errno_exit("write_file_error");
-	}
+  if (file_mjpeg != -1) {
+    if( -1 == write(file_mjpeg, data, length))
+      errno_exit("write_file_error");
+  }
 
 #endif
 
@@ -194,17 +207,17 @@ void write_vp8(char* data, int length,long timestamp) {
 
 #ifdef USE_FWRITE
 
-	add_vp8_frame_header(length,timestamp);	
-	
-	if (file_ivf != NULL) {
-		if( -1 == fwrite(data, length, 1, file_ivf))
-			errno_exit("write_file_error");
-	}	
+  add_vp8_frame_header(length,timestamp);
+
+  if (file_ivf != NULL) {
+    if( -1 == fwrite(data, length, 1, file_ivf))
+      errno_exit("write_file_error");
+  }
 #else
-	if (file_ivf != -1) {
-		if( -1 == write(file_ivf, data, length))
-			errno_exit("write_file_error");
-	}
+  if (file_ivf != -1) {
+    if( -1 == write(file_ivf, data, length))
+      errno_exit("write_file_error");
+  }
 
 #endif
 
@@ -213,130 +226,143 @@ void write_vp8(char* data, int length,long timestamp) {
 void write_log(char* str, int length)
 {
 #ifdef USE_FWRITE
-	if (file_log != NULL) {
-		if( -1 == fwrite(str, length, 1, file_log))
-			errno_exit("write_log_file_error");
-	}
+  if (file_log != NULL) {
+    if( -1 == fwrite(str, length, 1, file_log))
+      errno_exit("write_log_file_error");
+  }
 #else
-	if (file_log != -1) {
-		if( -1 == write(file_log, str, length))
-			errno_exit("write_log_file_error");
-	}
+  if (file_log != -1) {
+    if( -1 == write(file_log, str, length))
+      errno_exit("write_log_file_error");
+  }
 
 #endif
 }
 
 void close_file(void) {
 #ifdef USE_FWRITE
-	if (file_264 != NULL) {
-		fflush(file_264);
-		fclose(file_264);
-		file_264 = NULL;
-	}
-	if (file_yuv != NULL) {	
-		fflush(file_yuv);
-		fclose(file_yuv);
-		file_yuv = NULL;
-	}
+  if (file_264 != NULL) {
+    fflush(file_264);
+    fclose(file_264);
+    file_264 = NULL;
+  }
+  if (file_ts != NULL) {
+    fflush(file_ts);
+    fclose(file_ts);
+    file_ts = NULL;
+  }
+  if (file_yuv != NULL) {
+    fflush(file_yuv);
+    fclose(file_yuv);
+    file_yuv = NULL;
+  }
 
-	if (file_mjpeg != NULL) {	
-		fflush(file_mjpeg);
-		fclose(file_mjpeg);
-		file_mjpeg = NULL;
-	}
+  if (file_mjpeg != NULL) {
+    fflush(file_mjpeg);
+    fclose(file_mjpeg);
+    file_mjpeg = NULL;
+  }
 
-	if (file_ivf != NULL) {	
-	fflush(file_ivf);
-	fclose(file_ivf);
-	file_ivf = NULL;
-	}
+  if (file_ivf != NULL) {
+  fflush(file_ivf);
+  fclose(file_ivf);
+  file_ivf = NULL;
+  }
 
-	if (file_log != NULL) {
-		fflush(file_log);
-		fclose(file_log);
-		file_log = NULL;
-	}
+  if (file_log != NULL) {
+    fflush(file_log);
+    fclose(file_log);
+    file_log = NULL;
+  }
 #else
-	if (file_264 != -1) {
-		fsync(file_264);
-		close(file_264);
-		file_264 = -1;
-	}
-	if (file_yuv != -1) {
-		fsync(file_yuv);
-		close(file_yuv);
-		file_yuv = -1;
-	}
+  if (file_264 != -1) {
+    fsync(file_264);
+    close(file_264);
+    file_264 = -1;
+  }
+  if (file_ts != -1) {
+    fsync(file_ts);
+    close(file_ts);
+    file_ts = -1;
+  if (file_yuv != -1) {
+    fsync(file_yuv);
+    close(file_yuv);
+    file_yuv = -1;
+  }
 
-	if (file_mjpeg != -1) {
-		fsync(file_mjpeg);
-		close(file_mjpeg);
-		file_mjpeg = -1;
-	}
+  if (file_mjpeg != -1) {
+    fsync(file_mjpeg);
+    close(file_mjpeg);
+    file_mjpeg = -1;
+  }
 
-	if (file_ivf != -1) {	
-		fflush(file_ivf);
-		fclose(file_ivf);
-		file_ivf = -1;
-	}
-		
-	if (file_log != -1) {
-		fsync(file_log);
-		close(file_log);
-		file_log = -1;
-	}
+  if (file_ivf != -1) {
+    fflush(file_ivf);
+    fclose(file_ivf);
+    file_ivf = -1;
+  }
+
+  if (file_log != -1) {
+    fsync(file_log);
+    close(file_log);
+    file_log = -1;
+  }
 #endif
 }
 
 void open_file_dump(char* filename) {
 
-	char avc_name[128] = {0};
-	char yuv_name[128] = {0};	
-	char mjpeg_name[128] = {0};
-	char ivf_name[128] = {0};
-	
-	if (filename != NULL) {
-		snprintf(avc_name, 128, "%s.264", filename);
-		snprintf(yuv_name, 128, "%s.yuv", filename);
-		snprintf(mjpeg_name, 128, "%s.mjpg", filename);
-		snprintf(ivf_name, 128, "%s.ivf", filename);
-		
+  char avc_name[128] = {0};
+  char ts_name[128]  = {0};
+  char yuv_name[128] = {0};
+  char mjpeg_name[128] = {0};
+  char ivf_name[128] = {0};
+
+  if (filename != NULL) {
+    snprintf(avc_name, 128, "%s.264", filename);
+    snprintf(ts_name, 128, "%s.len", filename);
+    snprintf(yuv_name, 128, "%s.yuv", filename);
+    snprintf(mjpeg_name, 128, "%s.mjpg", filename);
+    snprintf(ivf_name, 128, "%s.ivf", filename);
+
 #ifdef USE_FWRITE
-		/*file = fopen (filename, "w");*/
-		file_264 = fopen (avc_name, "w");
-		file_yuv = fopen (yuv_name, "w");
-		file_mjpeg = fopen (mjpeg_name, "w");
-		file_ivf = fopen (ivf_name, "w");
-	
+    /*file = fopen (filename, "w");*/
+    file_264 = fopen (avc_name, "w");
+    file_ts = fopen (ts_name, "w");
+    file_yuv = fopen (yuv_name, "w");
+    file_mjpeg = fopen (mjpeg_name, "w");
+    file_ivf = fopen (ivf_name, "w");
+
 #else
-		/*file = open(filename, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);*/
-		file_264 = open(avc_name, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);
-		file_yuv = open(yuv_name, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);
-		file_mjpeg = open(mjpeg_name, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);
-		file_ivf = open(ivf_name, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);
+    /*file = open(filename, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);*/
+    file_264 = open(avc_name, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);
+    file_ts = open(ts_name, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);
+    file_yuv = open(yuv_name, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);
+    file_mjpeg = open(mjpeg_name, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);
+    file_ivf = open(ivf_name, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);
 
 #endif
 
-	}
+  }
 
 }
 
 void open_file_log(char* filename) {
 #ifdef USE_FWRITE
-	file_log = fopen (filename, "w");
+  file_log = fopen (filename, "w");
 #else
-	file_log = open(filename, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);
+  file_log = open(filename, O_CREAT|O_WRONLY|O_NONBLOCK, S_IRWXU|S_IRWXG|S_IRWXO);
 #endif
 }
 
 void ts_dump(int length, char*data) {
 
-	static FILE* fp = NULL;
-	if ( fp == NULL)
-		fp = fopen("./dump.ts", "w");
+  static FILE* fp = NULL;
+  if ( fp == NULL)
+    fp = fopen("./dump.ts", "w");
 
-	if( -1 == fwrite(data, length, 1, fp))
-		printf("ts_dump error: %s", strerror(errno));
+  if( -1 == fwrite(data, length, 1, fp))
+    printf("ts_dump error: %s", strerror(errno));
 
 }
 
